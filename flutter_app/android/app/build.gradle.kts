fb6714d0841bcd37723d79894a6d394c161fa977
Ayush Arya <ayusharya.25a@gmail.com>
2026-09-12 23:44:05 +0530

    fix: use flutter.ndkVersion instead of hardcoded NDK version

--- flutter_app/android/app/build.gradle.kts
+++ flutter_app/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.example.flutter_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "30.0.16248370"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
